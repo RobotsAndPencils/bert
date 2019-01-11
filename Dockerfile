65f72808bdb324e2c3888b1a540b721d848e972c
Ted Goddard <ted.goddard@robotsandpencils.com>
2019-01-11 11:40:42 -0700

    S3 trained models

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM ubuntu:18.04
-RUN apt-get update && apt-get clean
-RUN apt-get install -y curl
-RUN apt-get install -y python3-minimal python3-pip python3-distutils python3-setuptools && apt-get clean
-#RUN pip3 install wheel
+RUN apt-get update && apt-get clean && apt-get install -y curl unzip python3-minimal python3-pip
+RUN apt-get install -y curl unzip
 
 RUN pip3 install tensorflow==1.12.0
 RUN pip3 install flask
@@ -11,6 +9,15 @@ ENV BERT_HOME /usr/local/bert/
 RUN mkdir $BERT_HOME
 
 WORKDIR $BERT_HOME
+
+RUN curl -O https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+RUN unzip uncased_L-12_H-768_A-12.zip
+RUN rm uncased_L-12_H-768_A-12.zip
+
+#RUN curl -O https://s3-us-west-2.amazonaws.com/elasticallen/squad1_base.tar.gz
+RUN curl -O https://s3-us-west-2.amazonaws.com/elasticallen/squad2_base.tar.gz
+RUN tar xf squad2_base.tar.gz
+RUN rm squad2_base.tar.gz
 
 ##Optimize this later
 ADD . $BERT_HOME/
--- bert_server.py
+++ bert_server.py
@@ -3,22 +3,24 @@ from flask import Flask, request
 import tensorflow as tf
 import run_squad
 
+# curl -X POST http://localhost:5555/bert -H 'Content-Type: application/json' -d'{"question":"Who began life as a network protocol?", "passage":"Kermit is a frog, but he began life as a network protocol."}' 
+
 FLAGS = tf.flags.FLAGS
 
 app = Flask(__name__)
 
 @app.before_first_request
 def setup():
-    BERT_BASE_DIR = "../../dev-bert/uncased_L-12_H-768_A-12"
-    SQUAD_DIR = "../../dev-bert/"
+    BERT_BASE_DIR = "uncased_L-12_H-768_A-12"
+    SQUAD_DIR = "squad"
     
-    #    FLAGS.request_data = data
     FLAGS.predict_file = SQUAD_DIR + "/dev-v2.0.json"
     FLAGS.train_file = SQUAD_DIR + "/train-v2.0.json"
     FLAGS.init_checkpoint = BERT_BASE_DIR + "/bert_model.ckpt"
     FLAGS.bert_config_file = BERT_BASE_DIR + "/bert_config.json"
     FLAGS.vocab_file = BERT_BASE_DIR + "/vocab.txt"
-    FLAGS.output_dir = "/tmp/squad_base/"
+    #FLAGS.output_dir = "squad1_base"
+    FLAGS.output_dir = "squad2_base"
     FLAGS.train_batch_size=12
     FLAGS.learning_rate=3e-5
     FLAGS.num_train_epochs=2.0
